57ad800872eb23e5f6bde2aea8d032fab7b3ac96
dannyl1u <dannyjialiliu@gmail.com>
2023-11-09 12:38:24 -0800

    test auto call matching

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT || 9000;
-
-const server = app.listen(PORT);
-
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-});
-
+const server = app.listen(process.env.PORT || 9000);
+const peerServer = ExpressPeerServer(server, { debug: true });
 app.use('/peerjs', peerServer);
 
-console.log(`PeerJS server is running on port ${PORT}`);
+let waitingUsers = [];
+
+app.get('/pair', (req, res) => {
+  if (waitingUsers.length > 0) {
+    const peerId = waitingUsers.shift(); // Get the first waiting user
+    res.json({ peerId });
+  } else {
+    res.status(404).json({ message: 'No peers waiting' });
+  }
+});
+
+app.post('/wait', (req, res) => {
+  const { peerId } = req.body;
+  waitingUsers.push(peerId); // Add new user to the waiting list
+  res.status(200).json({ message: 'Added to waiting list' });
+});
+
+console.log(`Server is running on port ${process.env.PORT || 9000}`);
