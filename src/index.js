b9baa2b4c67ad1ac9aa5fc7301f205a710077da6
dannyl1u <dannyjialiliu@gmail.com>
2023-11-09 20:20:42 -0800

    waiting users array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const { ExpressPeerServer } = require('peer');
+
 const app = express();
+app.use(cors())
+app.use(express.json());
 
 const PORT = process.env.PORT || 9000;
 
@@ -8,9 +12,17 @@ const server = app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
+let waitingUsers = [];
+
 const peerServer = ExpressPeerServer(server, {
   debug: true,
   path: '/'
+});
+
+app.post('/addUserToWaitingList', (req, res) => {
+  waitingUsers.push(req.body.peerId); 
+  res.json({ message: `User id ${req.body.peerId} added to waiting list` }); 
+  console.log(waitingUsers);
 });
 
 app.use('/peerjs', peerServer);
