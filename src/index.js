ad32297602ddc92ba257b962c90337d2431f625f
dannyl1u <dannyjialiliu@gmail.com>
2023-11-09 16:07:07 -0800

    update /wait endpoint

--- src/index.js
+++ src/index.js
@@ -13,6 +13,15 @@ app.use('/peerjs', peerServer);
 
 let waitingUsers = [];
 
+peerServer.on('connection', (client) => {
+  console.log(`User connected with ID: ${client.getId()}`);
+});
+
+peerServer.on('disconnect', (client) => {
+  console.log(`User disconnected with ID: ${client.getId()}`);
+  waitingUsers = waitingUsers.filter(peerId => peerId !== client.getId());
+});
+
 app.get('/pair', (req, res) => {
   if (waitingUsers.length > 0) {
     const peerId = waitingUsers.shift(); // Get the first waiting user
@@ -24,7 +33,10 @@ app.get('/pair', (req, res) => {
 
 app.post('/wait', (req, res) => {
   const { peerId } = req.body;
-  waitingUsers.push(peerId); // Add new user to the waiting list
+  if (!waitingUsers.includes(peerId)) {
+    waitingUsers.push(peerId); // Add new user to the waiting list
+    console.log(`User with ID ${peerId} added to waiting list`);
+  }
   res.status(200).json({ message: 'Added to waiting list' });
 });
 
