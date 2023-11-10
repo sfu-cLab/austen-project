ea9d98af987e7a5e3ca96d40509022f22f504b01
dannyl1u <dannyjialiliu@gmail.com>
2023-11-09 16:35:06 -0800

    test basic voice call functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
 const app = express();
-const server = require('http').createServer(app);
+
 const PORT = process.env.PORT || 9000;
 
-// PeerJS server
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
 const peerServer = ExpressPeerServer(server, {
   debug: true,
-  path: '/myapp'
+  path: '/'
 });
 
 app.use('/peerjs', peerServer);
-
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
