621fcfd7bfc3b0b64d92adc8e2af5775d200f6d2
Danny Liu <dannyjialiliu@gmail.com>
2024-01-25 17:42:34 -0800

    send users schedule to client 📆

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
-
+const fs = require('fs').promises;
 const app = express();
 
 app.use(cors());
@@ -33,11 +33,21 @@ async function updateUserAvailability(userName, isAvailable) {
             return;
         }
         users[userIndex].isAvailable = isAvailable;
-        await fs.writeFile('./users.json', JSON.stringify(users));
+        await fs.writeFile('src/users.json', JSON.stringify(users));
         console.log('User availability updated for ' + userName + ' to ' + isAvailable);
     }
     catch (err) {
         console.log('Error updating user availability: ', err);
+    }
+}
+
+async function getUsers() {
+    try {
+        const data = await fs.readFile('src/users.json', 'utf8');
+        return JSON.parse(data);
+    }
+    catch (err) {
+        console.log('Error getting users: ', err);
     }
 }
 
@@ -68,32 +78,40 @@ function handleDisconnect(socket) {
     socket.emit('onlineUsers', userEmojis);
 }
 
-io.on('connection', (socket) => {
-    const assignedEmoji = assignEmoji();
-    if (assignedEmoji) {
-        userEmojis[socket.id] = assignedEmoji;
-        console.log(`Emoji ${assignedEmoji} assigned to user ${socket.id}`);
-        socket.emit('assignEmoji', assignedEmoji);
-    } else {
-        socket.emit('noEmojiAvailable');
+io.on('connection', async (socket) => {
+    try {
+        let users = await getUsers();
+        const assignedEmoji = assignEmoji();
+        if (assignedEmoji) {
+            userEmojis[socket.id] = assignedEmoji;
+            console.log(`Emoji ${assignedEmoji} assigned to user ${socket.id}`);
+            socket.emit('assignEmoji', assignedEmoji);
+        } else {
+            socket.emit('noEmojiAvailable');
+        }
+
+        socket.on('clientDisconnecting', () => {
+            handleDisconnect(socket);
+        });
+
+        socket.on('disconnect', () => {
+            handleDisconnect(socket);
+        });
+
+        socket.on('emojiClicked', (data) => {
+            console.log(data);
+            scheduleCall(data.slot, data.callSenderEmoji, data.callReceiverEmoji);
+            io.emit('newCallScheduled', data);
+        });
+
+        io.emit('users', users);
+        
+        io.emit('onlineUsers', users);
+    } catch (err) {
+        console.error('Error in connection handler:', err);
     }
-
-    socket.on('clientDisconnecting', () => {
-        handleDisconnect(socket);
-    });
-
-    socket.on('disconnect', () => {
-        handleDisconnect(socket);
-    });
-
-    socket.on('emojiClicked', (data) => {
-        console.log(data);
-        scheduleCall(data.slot, data.callSenderEmoji, data.callReceiverEmoji);
-        io.emit('newCallScheduled', data);
-    });
-    
-    io.emit('onlineUsers', userEmojis);
 });
+
 
 const path = require("path");
 const { ExpressPeerServer } = require("peer");
