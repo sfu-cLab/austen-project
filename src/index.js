86f2f745025c6cd6372f0bc078d6dcef63441dcb
dannyl1u <dannyjialiliu@gmail.com>
2024-03-14 05:36:34 -0700

    discord wip

--- src/index.js
+++ src/index.js
@@ -5,8 +5,85 @@ const socketConfig = require('./config/socketConfig');
 const routes = require('./routes');
 const { ExpressPeerServer } = require("peer");
 const path = require('path');
-
 const fs = require('fs');
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates
+    ]
+});
+
+const token = process.env.DISCORD_BOT_TOKEN;
+
+const USER_ID_1 = '1217803633580576878';
+const USER_ID_2 = '1040442081459589130';
+const USER_ID_3 = '723677031442677811';
+const USER_ID_4 = '';
+const USER_ID_5 = '';
+const USER_ID_6 = '';
+
+const VOICE_CHANNEL_ID_1 = '1217794085755289661';
+const VOICE_CHANNEL_ID_2 = '1217794115035463750';
+const VOICE_CHANNEL_ID_3 = '1217794115035463750';
+
+const emojiToUserIdMap = {
+  "📘": USER_ID_1,
+  "🦆": USER_ID_2,
+  "🌼": USER_ID_3,
+};
+
+let callsData = JSON.parse(fs.readFileSync('src/calls.json', 'utf-8'));
+const startTime = new Date().getTime();
+const durationMinutes = 1;
+
+const schedule = [
+    { time: 5000, caller: USER_ID_1, callee: USER_ID_3, channel: VOICE_CHANNEL_ID_1 },
+    { time: 10000, caller: USER_ID_1, callee: USER_ID_3, channel: VOICE_CHANNEL_ID_2 },
+    // Add more scheduled calls as needed
+];
+
+client.once('ready', () => {
+    console.log('Bot is ready.');
+
+    schedule.forEach(call => {
+        setTimeout(() => {
+            moveUsers(call.caller, call.callee, call.channel);
+        }, call.time);
+    });
+});
+
+async function moveUsers(callerId, calleeId, channelId) {
+    const guild = client.guilds.cache.first();
+    const channel = await guild.channels.fetch(channelId);
+    
+    [callerId, calleeId].forEach(async userId => {
+        const member = await guild.members.fetch(userId);
+        if (member && member.voice.channelId !== channelId) {
+            member.voice.setChannel(channel)
+                .then(() => console.log(`Moved ${member.user.username} to ${channel.name}.`))
+                .catch(console.error);
+              
+            if (member.voice.serverMute) {
+                await member.voice.setMute(false);
+                console.log(`Unmuted ${member.user.username}.`);
+            }
+        }
+    });
+}
+
+client.login(token);
+
+
+
+
+
+
+
+
+
 
 const app = express();
 const PORT = 443;
