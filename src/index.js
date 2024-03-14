573621f0a57f52879f464303219c08f967bf9240
dannyl1u <dannyjialiliu@gmail.com>
2024-03-14 06:25:59 -0700

    remove peerjs dependencies and setup discord calling wip

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const cors = require('cors');
 const https = require('https');
 const socketConfig = require('./config/socketConfig');
 const routes = require('./routes');
-const { ExpressPeerServer } = require("peer");
 const path = require('path');
 const fs = require('fs');
 require('dotenv').config();
 
+// discord setup and logic
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
     intents: [
@@ -25,35 +25,66 @@ const USER_ID_4 = '';
 const USER_ID_5 = '';
 const USER_ID_6 = '';
 
+const emojiToUserIdMap = {
+    "📘": USER_ID_1,
+    "🦆": USER_ID_2,
+    "🌼": USER_ID_3,
+};
+
+const timeslotsData = JSON.parse(fs.readFileSync('src/timeslots.json', 'utf-8'));
+  
 const VOICE_CHANNEL_ID_1 = '1217794085755289661';
 const VOICE_CHANNEL_ID_2 = '1217794115035463750';
 const VOICE_CHANNEL_ID_3 = '1217794115035463750';
 
-const emojiToUserIdMap = {
-  "📘": USER_ID_1,
-  "🦆": USER_ID_2,
-  "🌼": USER_ID_3,
-};
-
-let callsData = JSON.parse(fs.readFileSync('src/calls.json', 'utf-8'));
-const startTime = new Date().getTime();
-const durationMinutes = 1;
-
-const schedule = [
-    { time: 5000, caller: USER_ID_1, callee: USER_ID_3, channel: VOICE_CHANNEL_ID_1 },
-    { time: 10000, caller: USER_ID_1, callee: USER_ID_3, channel: VOICE_CHANNEL_ID_2 },
-    // Add more scheduled calls as needed
-];
+const startTime = new Date();
 
 client.once('ready', () => {
     console.log('Bot is ready.');
-
-    schedule.forEach(call => {
-        setTimeout(() => {
-            moveUsers(call.caller, call.callee, call.channel);
-        }, call.time);
-    });
+    monitorTimeslots();
 });
+
+function monitorTimeslots() {
+    const now = new Date();
+    console.log(now.getHours(), now.getMinutes());
+    console.log(`Checking timeslots at ${now.toISOString()}`);
+    timeslotsData.timeslots.forEach(timeslot => {
+
+        const [startHours, startMinutes] = timeslot.start.split(':').map(Number);
+        startTime.setHours(startHours, startMinutes, 0, 0);
+
+        const endTime = new Date(startTime);
+        const [endHours, endMinutes] = timeslot.end.split(':').map(Number);
+        endTime.setHours(endHours, endMinutes, 0, 0);
+
+        if (now >= startTime && now < endTime) {
+            let callsData = JSON.parse(fs.readFileSync('src/calls.json', 'utf-8'));
+            const currentCalls = callsData[timeslot.timeslot];
+
+            console.log(`Current timeslot: ${timeslot.timeslot} - setting up calls: ${currentCalls}`);
+
+            var callCount = 0;
+
+            currentCalls.forEach(call => {
+                const callerId = emojiToUserIdMap[call.callerEmoji];
+                const calleeId = emojiToUserIdMap[call.calleeEmoji];
+
+                if (callCount == 0) {
+                    moveUsers(callerId, calleeId, VOICE_CHANNEL_ID_1);
+                }
+                else if (callCount == 1) {
+                    moveUsers(callerId, calleeId, VOICE_CHANNEL_ID_2);
+                }
+                else if (callCount == 2) {
+                    moveUsers(callerId, calleeId, VOICE_CHANNEL_ID_3);
+                }
+
+                callCount++;
+            });
+        }
+    });
+    setTimeout(monitorTimeslots, 5000);
+}
 
 async function moveUsers(callerId, calleeId, channelId) {
     const guild = client.guilds.cache.first();
@@ -76,15 +107,7 @@ async function moveUsers(callerId, calleeId, channelId) {
 
 client.login(token);
 
-
-
-
-
-
-
-
-
-
+// server setup - express, sockets, certs
 const app = express();
 const PORT = 443;
 
@@ -106,17 +129,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-
 const io = require('socket.io')(server, socketConfig);
 require('./controllers/socketController')(io);
 
-const peerServer = ExpressPeerServer(server, {
-  proxied: true,
-  debug: true,
-  path: "/myapp",
-});
-
-app.use(peerServer);
 app.use(express.static(path.join(__dirname)));
 
 server.listen(PORT, () => {
